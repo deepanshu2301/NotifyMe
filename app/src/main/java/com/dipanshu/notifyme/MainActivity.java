5aab64e415518080e96db8a90821b1c9f8578445
deepanshu2301 <deepanshujindal40@gmail.com>
2019-08-27 19:31:09 +0530

    Code

--- app/src/main/java/com/dipanshu/notifyme/MainActivity.java
+++ app/src/main/java/com/dipanshu/notifyme/MainActivity.java
@@ -4,7 +4,13 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +19,7 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -26,6 +33,42 @@ public class MainActivity extends Activity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        SensorManager sensorManager =
+                (SensorManager) getSystemService(SENSOR_SERVICE);
+        assert sensorManager != null;
+        final Sensor proximitySensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        // Create listener
+        SensorEventListener proximitySensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // More code goes here
+                if(sensorEvent.values[0] < proximitySensor.getMaximumRange()) {
+                    // Detected something nearby
+                    Log.e("Val=", String.valueOf(sensorEvent.values[0]));
+
+                    NotificationManager notificationManager = (NotificationManager)
+                            getSystemService(Context.
+                                    NOTIFICATION_SERVICE);
+                    assert notificationManager != null;
+                    notificationManager.cancelAll();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        // Register it, specifying the polling interval in
+// microseconds
+        sensorManager.registerListener(proximitySensorListener,
+                proximitySensor, 1000*100);
+
+
         Button btn = findViewById(R.id.btn);
 
         final NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -39,6 +82,18 @@ public class MainActivity extends Activity  {
         final EditText title=findViewById(R.id.input_edittitle);
         final EditText message=findViewById(R.id.input_editmessage);
 
+
+        Button del=findViewById(R.id.btndel);
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(Context.
+                                NOTIFICATION_SERVICE);
+                assert notificationManager != null;
+                notificationManager.cancelAll();            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
